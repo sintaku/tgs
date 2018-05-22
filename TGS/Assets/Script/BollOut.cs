9ddacaffa6b29d5817ea601f38178c78c99be4af
sintaku <55sintaku@gmail.com>
2018-05-22 21:50:01 +0900

    master

---
+++ TGS/Assets/Script/BollOut.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BollOut : MonoBehaviour {
+
+    public GameObject bollPre;
+    private float timeOut;
+    public float BollOutTime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timeOut += Time.deltaTime;
+
+        if (timeOut>=BollOutTime)
+        {
+            Instantiate(bollPre, transform.position, Quaternion.identity);
+
+            timeOut = 0.0f;
+
+            
+        }
+		
+	}
+}
